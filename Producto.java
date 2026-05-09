21958f60bbc7a4560ef5ed7ac85391278f496647
Stefany Barba G <20253RD024@utez.edu.mx>
2026-05-08 19:56:29 -0600

    Producto.java

--- Producto.java
+++ Producto.java
@@ -1,9 +1,9 @@
 public class Producto {
 
     // Atributos
-    String nombre;
-    double precio;
-    int stock;
+    private String nombre;
+    private double precio;
+    private int stock;
 
     // Constructor
     public Producto(String nombre, double precio, int stock) {
@@ -12,24 +12,24 @@ public class Producto {
         this.stock = stock;
     }
 
-    // Método para mostrar información
+    // Metodo para mostrar informacion
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Precio: $" + precio);
         System.out.println("Stock: " + stock);
     }
 
-    // Método para calcular valor del inventario
+    // Metodo para calcular valor del inventario
     public double calcularValorInventario() {
         return precio * stock;
     }
 
-    // Método para aplicar descuento
+    // Metodo para aplicar descuento
     public void aplicarDescuento(double porcentaje) {
         precio = precio - (precio * porcentaje / 100);
     }
 
-    // Método para verificar disponibilidad
+    // Metodo para verificar disponibilidad
     public void hayDisponibilidad() {
         if (stock > 0) {
             System.out.println("Hay disponibilidad");
